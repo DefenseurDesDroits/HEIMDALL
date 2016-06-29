335e1411b91cd823c2c11b6d4aadeed230f16c64
MagnusMoi <travail@emetal.fr>
2016-06-29 16:13:55 +0200

    WIP Javascript Génération des classes
    Première génération des classes en JS.
    En attente de relecture., d'assemblage avec les DAO PHP

---
+++ main.js
@@ -0,0 +1,11 @@
+//Module : None
+//Created by : Ludo Lib JsPhp
+//Generated on : 2016-06-29 04:04:05
+//Filename : main.js
+//Description : Main file of our main app
+
+///[METHOD][main]Our main function
+function main(){
+	//To your job
+	alert("It's working bro");
+};
